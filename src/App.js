0cb58de5845d85b3edba2b0fe70f8ac8d20761bb
Joshwa-shahbaz <joshwashahbaz2@gmail.com>
2023-05-09 01:44:24 -0700

    correction of errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Hook from './Componenets/Hook';
 import Practice from './Componenets/Practice';
 
 function App() {
   return (
     <div className="App">
-      {/* <Hook/> */}
       <Practice />  
     </div>
   );
